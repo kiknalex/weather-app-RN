9a5ba3bf2ccbea29974bbc883904766f40b0e3e4
Alex <aleksandrekiknadzee@gmail.com>
2025-03-27 21:39:52 +0400

    fix: move global.css import into a Root Route

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { store } from "../store/store";
 import { Provider } from "react-redux";
+import "../../global.css";
 export default function RootLayout() {
 	return (
 		<Provider store={store}>
