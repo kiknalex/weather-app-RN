8bed22be3f7005b62b1c5bdbb278b8b34e1059d7
Alex <aleksandrekiknadzee@gmail.com>
2025-03-28 18:16:12 +0400

    feat: add styles to header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,10 +2,22 @@ import { Stack } from "expo-router";
 import { store } from "../store/store";
 import { Provider } from "react-redux";
 import "../../global.css";
+
+const screenOptions = {
+	headerStyle: {
+		backgroundColor: "#f4511e",
+	},
+	headerTintColor: "#fff",
+	headerTitleStyle: {
+		fontWeight: "bold" as "bold",
+	},
+	headerTitle: "Home",
+};
+
 export default function RootLayout() {
 	return (
 		<Provider store={store}>
-			<Stack />
+			<Stack screenOptions={screenOptions} />
 		</Provider>
 	);
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
 import { View } from "react-native";
 import NewsGeneralList from "../features/news-general-list/news-general-list";
+import { Stack } from "expo-router";
 
 export default function HomeScreen() {
 	return (
 		<View>
+			<Stack.Screen name="home" />
 			<NewsGeneralList />
 		</View>
 	);
